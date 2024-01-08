b8a7e58c6c3ce0b0ac7c1cc6c42ca5da1b4edd53
alldaygzchen <gonzalochen66@gmail.com>
2024-01-08 22:41:06 +0800

    first commit

---
+++ src/components/Markdown.jsx
@@ -0,0 +1,57 @@
+import PropTypes from "prop-types";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const Markdown = ({ markdownText }) => {
+  const handleCopyToClipboard = async (codeContext) => {
+    try {
+      // Use navigator.clipboard.writeText to copy content to clipboard
+      await navigator.clipboard.writeText(codeContext);
+      alert("Content copied to clipboard!");
+    } catch (error) {
+      console.error("Unable to copy to clipboard", error);
+    }
+  };
+
+  return (
+    <ReactMarkdown
+      components={{
+        code({ inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "language-js");
+          const codeContext = String(children).replace(/\n$/, "");
+          return !inline ? (
+            <div className="relative">
+              <button
+                className="bg-blue-500 rounded-lg text-black text-right absolute top-0 right-0"
+                onClick={() => handleCopyToClipboard(codeContext)}
+              >
+                Copy code
+              </button>
+              <SyntaxHighlighter
+                {...props}
+                style={atomDark}
+                language={match[1]}
+                PreTag="div"
+              >
+                {codeContext}
+              </SyntaxHighlighter>
+            </div>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
+      {markdownText}
+    </ReactMarkdown>
+  );
+};
+
+export default Markdown;
+
+Markdown.propTypes = {
+  markdownText: PropTypes.string,
+};
